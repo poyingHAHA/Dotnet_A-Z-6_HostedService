e8e023d7d1a54dd8bd99fe0d2b6d0bcc4083ea4b
poyingHAHA <poying.code@gmail.com>
2022-08-01 20:07:50 +0800

    Validation1 - 內置數據驗證規則using System.ComponentModel.DataAnnotations;

---
+++ HostedService1/HostedService1/AddNewUserRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HostedService1
+{
+    public class AddNewUserRequest
+    {
+        [MinLength(10)]
+        public string UserName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Password { get; set; }
+        [Compare(nameof(Password))]
+        public string Password2 { get; set; }
+    }
+}
---
+++ HostedService1/HostedService1/Controllers/DemoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HostedService1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult Register(AddNewUserRequest req)
+        {
+            return Ok(req);
+        }
+    }
+}
