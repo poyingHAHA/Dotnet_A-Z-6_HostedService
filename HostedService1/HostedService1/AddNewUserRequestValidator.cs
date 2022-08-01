22575c04e078ee5d4bc3c80a48bd190493b57900
poyingHAHA <poying.code@gmail.com>
2022-08-01 21:01:21 +0800

    Validation3 - 在FluentValidation注入其他服務ex.DbContext

--- HostedService1/HostedService1/AddNewUserRequestValidator.cs
+++ HostedService1/HostedService1/AddNewUserRequestValidator.cs
@@ -1,13 +1,28 @@
 ﻿using FluentValidation;
+using Microsoft.AspNetCore.Identity;
 
 namespace HostedService1
 {
     public class AddNewUserRequestValidator: AbstractValidator<AddNewUserRequest>
     {
+        private readonly UserManager<MyUser> userManager;
+
+        public AddNewUserRequestValidator(UserManager<MyUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
         public AddNewUserRequestValidator()
         {
-            RuleFor(x => x.Email).NotNull().EmailAddress().WithMessage("必須是合法郵箱").Must(x => x.EndsWith("@gmail.com") || x.EndsWith("@edu.com.tw")).WithMessage("Email只能是gmail或教育信箱");
-            RuleFor(x => x.UserName).NotNull().Length(3, 10).WithMessage("密碼要界於3~10");
+            RuleFor(x => x.Email).NotNull()
+                .EmailAddress()
+                .WithMessage("必須是合法郵箱")
+                .Must(x => x.EndsWith("@gmail.com") || x.EndsWith("@edu.com.tw")).WithMessage("Email只能是gmail或教育信箱");
+            RuleFor(x => x.UserName).NotNull()
+                .Length(3, 10)
+                .WithMessage("密碼要界於3~10")
+                .MustAsync(async (x,_)=> await userManager.FindByIdAsync(x)==null)
+                .WithMessage(x => $"{x.UserName}已經存在");
             RuleFor(x => x.Password).Equal(x => x.Password2).WithMessage("密碼要一致");
         }
     }
